f855722c0b06a24085269413eff533b9f39eddc2
sabasharat.ali@gmail.com <sabasharat.ali@gmail.com>
2020-07-17 19:08:04 +0500

    added default functionalities using navigation

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,19 +1,32 @@
+import { Platform } from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
 
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
 import CategoriesScreen from "../screens/CategoriesScreen.js";
+import Colors from "../constants/colors.js";
 
-const MealsNavigator = createStackNavigator({
-  Categories: {
-    screen: CategoriesScreen,
+const MealsNavigator = createStackNavigator(
+  {
+    Categories: {
+      screen: CategoriesScreen,
+    },
+    CategoryMeals: {
+      screen: CategoryMealsScreen,
+    },
+    MealDetail: {
+      screen: MealDetailScreen,
+    },
   },
-  CategoryMeals: {
-    screen: CategoryMealsScreen,
-  },
-  MealDetail: {
-    screen: MealDetailScreen,
-  },
-});
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
+      },
+      headerTintColor:
+        Platform.OS === "android" ? "white" : Colors.primaryColor,
+    },
+  }
+);
 
 export default createAppContainer(MealsNavigator);
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,23 +1,43 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
 
 import { CATEGORIES } from "../data/dummy-data";
 
 const CategoryMealScreen = (props) => {
   const catId = props.navigation.getParam("categoryId");
+
   const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+
   return (
     <View style={styles.screen}>
       <Text>The Category Meal Screen!</Text>
       <Text>{selectedCategory.title}</Text>
       <Button
-        title="Meal Details"
+        title="Go to Details"
         onPress={() => {
-          props.navigation.navigate({ routeName: "MealDetail" });
+          props.navigation.navigate({
+            routeName: "MealDetail",
+          });
+        }}
+      />
+      <Button
+        title="Go Back"
+        onPress={() => {
+          props.navigation.pop();
         }}
       />
     </View>
   );
+};
+
+CategoryMealScreen.navigationOptions = (navigationData) => {
+  const catId = navigationData.navigation.getParam("categoryId");
+
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+
+  return {
+    headerTitle: selectedCategory.title,
+  };
 };
 
 const styles = StyleSheet.create({
