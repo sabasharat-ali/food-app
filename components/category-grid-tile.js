33971cc905507b7a6d589e617b9265061ea3a081
sabasharat.ali@gmail.com <sabasharat.ali@gmail.com>
2020-07-18 23:35:47 +0500

    added a new component

---
+++ components/category-grid-tile.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import { View } from "react-native";
+
+const CategoryGridTile = (props) => {
+  return (
+    <TouchableOpacity
+      style={styles.gridItem}
+      onPress={() => {
+        props.navigation.navigate({
+          routeName: "CategoryMeals",
+          params: { categoryId: itemData.item.id },
+        });
+      }}
+    >
+      <View>
+        <Text>{itemData.item.title}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
+  },
+});
+
+export default categoryGridTile;
