938ee9d8a580d98df3db7a3de71416d9b9cc276f
sabasharat.ali@gmail.com <sabasharat.ali@gmail.com>
2020-07-23 23:44:14 +0500

    styled the catefory meals screen

--- components/meal-item.js
+++ components/meal-item.js
@@ -9,14 +9,19 @@ import {
 
 const MealItem = (props) => {
   return (
-    <TouchableNativeFeedback onPress={props.onSelectMeal}>
-      <View style={styles.mealItem}>
+    <TouchableNativeFeedback
+      onPress={props.onSelectMeal}
+      style={styles.mealItem}
+    >
+      <View>
         <View style={{ ...styles.mealRow, ...styles.mealHeader }}>
-          <ImageBackground
-            source={{ uri: props.image }}
-            style={styles.bgImage}
-          />
-          <Text>{props.title}</Text>
+          <ImageBackground source={{ uri: props.image }} style={styles.bgImage}>
+            <View style={styles.titleContainer}>
+              <Text style={styles.title} numberOfLines={1}>
+                {props.title}
+              </Text>
+            </View>
+          </ImageBackground>
         </View>
         <View style={{ ...styles.mealRow, ...styles.mealDetail }}>
           <Text>{props.duration}m</Text>
@@ -32,21 +37,37 @@ const styles = StyleSheet.create({
   bgImage: {
     width: "100%",
     height: "100%",
+    justifyContent: "flex-end",
   },
   mealItem: {
     height: 200,
     width: "100%",
-    backgroundColor: "#ccc",
+    backgroundColor: "#f5f5f5",
+    borderRadius: 10,
+    overflow: "hidden",
   },
   mealRow: {
     flexDirection: "row",
   },
   mealHeader: {
-    height: "90%",
+    height: "85%",
   },
   mealDetail: {
     paddingHorizontal: 10,
     justifyContent: "space-between",
+    alignItems: "center",
+    height: "15",
+  },
+  titleContainer: {
+    backgroundColor: "rbga(0,0,0,0.5)",
+    paddingVertical: "5",
+    paddingHorizontal: "12",
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 22,
+    color: "white",
+    textAlign: "center",
   },
 });
 
