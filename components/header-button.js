d8db64b4a41eff1779b7080e29ea1fdb2cdabe48
sabasharat.ali@gmail.com <sabasharat.ali@gmail.com>
2020-07-29 23:48:24 +0500

    added a new component

---
+++ components/header-button.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { platform } from "react-native";
+import { HeaderButton } from "react-navigation-header-buttons";
+import { Ionicons } from "@expo/vector-icons";
+
+import Colors from "../constants/colors";
+
+const CustomHeaderbutton = (props) => {
+  return (
+    <HeaderButton
+      {...props}
+      IconComponent={Ionicons}
+      iconSize={23}
+      color={Platform.OS === "android" ? "white" : Colors.primaryColor}
+    />
+  );
+};
+
+export default CustomHeaderbutton;
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,23 +1,34 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
 
 import { MEALS } from "../data/dummy-data";
 
 const MealDetailScreen = (props) => {
   const mealId = props.navigation.getParam("mealId");
+
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
   return (
     <View style={styles.screen}>
       <Text>{selectedMeal.title}</Text>
+      <Button
+        title="Go Back to Categories"
+        onPress={() => {
+          props.navigation.popToTop();
+        }}
+      />
     </View>
   );
 };
 
 MealDetailScreen.navigationOptions = (navigationData) => {
-  const mealId = props.navigation.getParam("mealId");
+  const mealId = navigationData.navigation.getParam("mealId");
+
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
   return {
     headerTitle: selectedMeal.title,
+    headerRight: <Text>FAV</Text>,
   };
 };
 
