9f97730b41dd4097e08d647e955f9fdd652eab7e
sabasharat.ali@gmail.com <sabasharat.ali@gmail.com>
2020-08-18 15:00:33 +0500

    added a default container

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -20,6 +20,12 @@ const defaultStackNavOptions = {
   headerStyle: {
     backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
   },
+  headerTitleStyle: {
+    fontFamily: "open-sans-bold",
+  },
+  headerBackTitleStyle: {
+    fontFamily: "open-sans",
+  },
   headerTintColor: Platform.OS === "android" ? "white" : Colors.primaryColor,
   headerTitle: "A Screen",
 };
